28f45a7b1cf3405fc8825635b1fb901400ad602a
darshandnagesh <darshan.nagesh@sdworx.com>
2025-04-08 16:44:32 +0530

    Add HTML report generation to first-script.js

--- tests/first-script.js
+++ tests/first-script.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 export const options = {
     stages: [{ target: 20, duration: '1m' }],
     thresholds: {
@@ -11,4 +12,8 @@ export default function () {
     const resp = http.get('https://gorest.co.in/public/v2/users');
     check(resp, { 'status is 200': (r) => r.status === 200 });
     sleep(1);
+
+    return {
+        "result.html": htmlReport(data),
+    };
 }
